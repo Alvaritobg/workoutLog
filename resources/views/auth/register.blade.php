d18ab00376d4a9abeb7ce93afe535536e51e8bd9
ÁlvaroBG <alvaritobg@gmail.com>
2024-01-15 19:55:27 +0100

    Primer commit del proyecto con login y roles y permisos

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,76 @@
+<x-guest-layout>
+    <form method="POST" action="{{ route('register') }}">
+        @csrf
+
+        <!-- Name -->
+        <div>
+            <x-input-label for="name" :value="__('Nombre')" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
+                autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        </div>
+
+        <!-- Surname -->
+        <div>
+            <x-input-label for="surname" :value="__('Apellidos')" />
+            <x-text-input id="surname" class="block mt-1 w-full" type="text" name="surname" :value="old('surname')"
+                required autofocus autocomplete="surname" />
+            <x-input-error :messages="$errors->get('surname')" class="mt-2" />
+        </div>
+
+        <!-- Weight -->
+        <div>
+            <x-input-label for="weight" :value="__('Peso (Kg)')" />
+            <x-text-input id="weight" class="block mt-1 w-full" type="number" name="weight" :value="old('weight')"
+                required autofocus autocomplete="weight" />
+            <x-input-error :messages="$errors->get('weight')" class="mt-2" />
+        </div>
+
+        <!-- Date of birth -->
+        <div>
+            <x-input-label for="date_of_birth" :value="__('Fecha de nacimiento')" />
+            <x-text-input id="date_of_birth" class="block mt-1 w-full" type="date" name="date_of_birth"
+                :value="old('date_of_birth')" required autofocus autocomplete="date_of_birth" />
+            <x-input-error :messages="$errors->get('date_of_birth')" class="mt-2" />
+        </div>
+
+        <!-- Email Address -->
+        <div class="mt-4">
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                required autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
+
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                autocomplete="new-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <!-- Confirm Password -->
+        <div class="mt-4">
+            <x-input-label for="password_confirmation" :value="__('Confirmar Password')" />
+
+            <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
+                name="password_confirmation" required autocomplete="new-password" />
+
+            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                href="{{ route('login') }}">
+                {{ __('¿Ya tienes cuenta?') }}
+            </a>
+
+            <x-primary-button class="ms-4">
+                {{ __('Registrar') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout>
