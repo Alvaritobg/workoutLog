1d6d50b5cd75b6e7ec1393e8e9269e5e02720c2e
Alvaro <alvaro.banon@bntbusiness.es>
2024-02-16 10:20:19 +0100

    solucionado descuadre de rutinas en firefox

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -48,41 +48,35 @@
                         @endif
                     @endauth
                     {{-- SEGUNDA OPCIÓN --}}
-                    @auth
-                        @if (auth()->user()->hasRole('user'))
-                            <div class="basis-0 md:basis-5/12 grow">
-                                {{-- Asignación de la imagen de la rutina a una variable PHP --}}
-                                @php
-                                    //$routineImg = $routine->img;
-                                @endphp
 
-                                {{-- Enlace para cada rutina --}}
-                                <a href="{{ 'rutinas' }}">
-                                    {{-- Contenedor de la rutina --}}
+                    <div class="basis-0 md:basis-5/12 grow">
 
-                                    <div class="max-w-7xl mx-auto ">
-                                        {{-- Tarjeta de rutina con imagen de fondo y texto --}}
-                                        <div class="flex flex-col text-end text-white p-6 max-h-60 overflow-hidden shadow-md rounded-sm   
+                        {{-- Enlace para cada rutina --}}
+                        <a href="{{ 'rutinas' }}">
+                            {{-- Contenedor de la rutina --}}
+
+                            <div class="max-w-7xl mx-auto ">
+                                {{-- Tarjeta de rutina con imagen de fondo y texto --}}
+                                <div class="flex flex-col text-end text-white p-6 max-h-60 overflow-hidden shadow-md rounded-sm   
                                         h-screen bg-cover bg-center hover:grayscale"
-                                            style="background-image: url('{{ asset('img/pesas.jpg') }}')">
-                                            {{-- Información de la rutina --}}
-                                            <div class="flex flex-col justify-end flex-grow">
-                                                {{-- Nombre de la rutina --}}
-                                                <h3
-                                                    class="font-bold text-4xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] border-black">
-                                                    Rutinas</h3>
-                                                {{-- Descripción de la rutina --}}
-                                                <p
-                                                    class="font-extralightdrop-shadow-lg drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-                                                    Seleccione su rutina de entrenamiento</p>
-                                                </p>
-                                            </div>
-                                        </div>
+                                    style="background-image: url('{{ asset('img/pesas.jpg') }}')">
+                                    {{-- Información de la rutina --}}
+                                    <div class="flex flex-col justify-end flex-grow">
+                                        {{-- Nombre de la rutina --}}
+                                        <h3
+                                            class="font-bold text-4xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] border-black">
+                                            Rutinas</h3>
+                                        {{-- Descripción de la rutina --}}
+                                        <p
+                                            class="font-extralightdrop-shadow-lg drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+                                            Seleccione su rutina de entrenamiento</p>
+                                        </p>
                                     </div>
-                                </a>
+                                </div>
                             </div>
-                        @endif
-                    @endauth
+                        </a>
+                    </div>
+
                     {{-- TERCERA OPCION --}}
                     @auth
                         @if (auth()->user()->hasRole('user'))
--- resources/views/routines/index.blade.php
+++ resources/views/routines/index.blade.php
@@ -10,52 +10,57 @@
         {{-- Subtítulo o descripción adicional --}}
         <p>Seleccione la rutina que quiera realizar</p>
     </x-slot>
+    <div class="py-3 ">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                {{-- Contenedor principal --}}
+                {{-- Verifica si hay rutinas disponibles --}}
+                @if ($routines)
 
-    {{-- Contenedor principal --}}
-    {{-- Verifica si hay rutinas disponibles --}}
-    @if ($routines)
-
-        {{-- Bucle para iterar sobre cada rutina --}}
-        <div class="flex flex-col lg:flex-row flex-wrap py-2 px-2 md:px-5 my-4 gap-4 justify-around">
-            @foreach ($routines as $routine)
-                <div class="basis-0 md:basis-5/12 grow">
+                    {{-- Bucle para iterar sobre cada rutina --}}
+                    <div class="flex flex-col lg:flex-row flex-wrap py-2 px-2 md:px-5 my-4 gap-4 justify-around">
+                        @foreach ($routines as $routine)
+                            <div class="basis-0 md:basis-5/12 grow">
 
 
-                    {{-- Enlace para cada rutina --}}
-                    <a href="{{ route('routine.show', ['id' => $routine->id]) }}">
-                        {{-- Contenedor de la rutina --}}
-                        {{-- <div class="py-2 px-0 md:px-5 lg:px-10 xl:px-20"> --}}
-                        <div class="max-w-7xl mx-auto ">
-                            {{-- Tarjeta de rutina con imagen de fondo y texto --}}
-                            <div class="flex flex-col text-end text-white p-6 max-h-60 overflow-hidden shadow-md rounded-sm 
+                                {{-- Enlace para cada rutina --}}
+                                <a href="{{ route('routine.show', ['id' => $routine->id]) }}">
+                                    {{-- Contenedor de la rutina --}}
+                                    {{-- <div class="py-2 px-0 md:px-5 lg:px-10 xl:px-20"> --}}
+                                    <div class="max-w-7xl mx-auto ">
+                                        {{-- Tarjeta de rutina con imagen de fondo y texto --}}
+                                        <div class="flex flex-col text-end text-white p-6 max-h-60 overflow-hidden shadow-md rounded-sm 
                     
                     h-screen bg-cover bg-center hover:grayscale"
-                                style="background-image: url('{{ $routine->img }}')">
-                                {{-- Información de la rutina --}}
-                                <div class="flex flex-col justify-end flex-grow">
-                                    {{-- Nombre de la rutina --}}
-                                    <h3
-                                        class="font-bold text-4xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] border-black">
-                                        {{ $routine->name }}</h3>
-                                    {{-- Descripción de la rutina --}}
-                                    <p
-                                        class="font-extralightdrop-shadow-lg drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-                                        {{ $routine->description }}</p>
-                                    {{-- Información del entrenador de la rutina --}}
-                                    <p class="font-thin text-xs drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-                                        Trainer:
-                                        {{ $routine->user->name }} {{ $routine->user->surname }}</p>
-                                </div>
+                                            style="background-image: url('{{ $routine->img }}')">
+                                            {{-- Información de la rutina --}}
+                                            <div class="flex flex-col justify-end flex-grow">
+                                                {{-- Nombre de la rutina --}}
+                                                <h3
+                                                    class="font-bold text-4xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] border-black">
+                                                    {{ $routine->name }}</h3>
+                                                {{-- Descripción de la rutina --}}
+                                                <p
+                                                    class="font-extralightdrop-shadow-lg drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+                                                    {{ $routine->description }}</p>
+                                                {{-- Información del entrenador de la rutina --}}
+                                                <p
+                                                    class="font-thin text-xs drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+                                                    Trainer:
+                                                    {{ $routine->user->name }} {{ $routine->user->surname }}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    {{-- </div> --}}
+                                </a>
                             </div>
-                        </div>
-                        {{-- </div> --}}
-                    </a>
-                </div>
-            @endforeach
+                        @endforeach
+                    </div>
+                @else
+                    {{-- Mensaje si no hay rutinas disponibles --}}
+                    <p>No se encontraron ejercicios para este entrenamiento.</p>
+                @endif
+            </div>
         </div>
-    @else
-        {{-- Mensaje si no hay rutinas disponibles --}}
-        <p>No se encontraron ejercicios para este entrenamiento.</p>
-    @endif
-
+    </div>
 </x-app-layout>
